bc6c8b73d69b645fcce6ac3e22a52a0aa0b4e795
asztrikx <asztrik35@gmail.com>
2023-02-13 19:40:54 +0100

    fix not calling followSpeed

--- train-system/src/main/java/hu/bme/mit/train/system/TrainSystem.java
+++ train-system/src/main/java/hu/bme/mit/train/system/TrainSystem.java
@@ -6,8 +6,19 @@ import hu.bme.mit.train.interfaces.TrainSensor;
 import hu.bme.mit.train.interfaces.TrainUser;
 import hu.bme.mit.train.sensor.TrainSensorImpl;
 import hu.bme.mit.train.user.TrainUserImpl;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class TrainSystem {
+	TrainSystem() {
+		TimerTask task = new TimerTask() {
+			public void run() {
+				controller.followSpeed();
+			}
+		};
+		Timer timer = new Timer("Timer");
+		timer.schedule(task, 1000L);
+	}
 
 	private TrainController controller = new TrainControllerImpl();
 	private TrainUser user = new TrainUserImpl(controller);
